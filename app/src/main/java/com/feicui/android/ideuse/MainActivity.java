bd3d6ed4d2d44e3ea5f56352420b4028ec0cf24c
lijiarong <526901463@qq.com>
2016-10-19 17:07:45 +0800

    第二次提交

--- app/src/main/java/com/feicui/android/ideuse/MainActivity.java
+++ app/src/main/java/com/feicui/android/ideuse/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    String string = "23455";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
